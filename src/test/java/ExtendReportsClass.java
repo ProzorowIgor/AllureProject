0f46132d7b5420b3a385178c5607a98394582185
Igor <igor.prozorow@qualitestgroup.com>
2022-03-17 18:31:36 +0200

    sdf

---
+++ src/test/java/ExtendReportsClass.java
@@ -0,0 +1,21 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.testng.annotations.Test;
+
+public class ExtendReportsClass {
+
+    @Test
+public void extendTest(){
+        ExtentReports extent = new ExtentReports();
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+        extent.attachReporter(spark);
+        extent.createTest("MyFirstTest")
+                .log(Status.PASS, "This is a logging event for MyFirstTest, and it passed!");
+        extent.flush();
+
+    }
+
+
+
+}
